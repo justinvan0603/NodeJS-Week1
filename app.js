84b755c5a686635315e08bd191e8e24ef0bd63bb
JustinVan <jtvan13520776@gmail.com>
2019-05-02 21:53:51 +0200

    no message

--- app.js
+++ app.js
@@ -59,15 +59,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/study',studyRouter);
 
 app.use('/login',loginRouter);
 
-app.use(passport.initialize());
-app.use(passport.session());
+
 
 //app.use('/api/study',studyApiRouter);
 var GOOGLE_CLIENT_ID      = "391534551840-qi2p9he2al8i7ehvt5jid2b72paq4qkg.apps.googleusercontent.com"
