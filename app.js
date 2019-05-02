0822a79cb5e76e72f53571e9761cdc8e676c3cc9
JustinVan <jtvan13520776@gmail.com>
2019-05-02 21:35:54 +0200

    fix url

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ var GOOGLE_CLIENT_ID      = "391534551840-qi2p9he2al8i7ehvt5jid2b72paq4qkg.apps.
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
+  callbackURL: "https://week-1-demo.herokuapp.com/auth/google/callback"
 },
 function(accessToken, refreshToken, profile, done) {
        return done(null, user);
