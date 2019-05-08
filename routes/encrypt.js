2cc63897285295a1d6825002c7e01151681c935d
JustinVan <jtvan13520776@gmail.com>
2019-05-08 12:55:15 +0200

    Adding API decrypt

--- routes/encrypt.js
+++ routes/encrypt.js
@@ -6,5 +6,9 @@ router.get('/',function(req,res){
     var encryptedStr = cryptor.encryptWithRSAPublicKey("abc");
     res.send(encryptedStr.toString("base64"));
 });
-
+router.get('/decrypt',function(req,res){
+    var encryptedStr = req.params.id;
+    var decryptedStr = cryptor.decryptWithRSAPrivateKey(encryptedStr);
+    res.send(decryptedStr);
+});
 module.exports = router;
