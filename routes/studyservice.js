7dd0d8e8e7980cd14be1925a483c4b4ea9d96f35
JustinVan <jtvan13520776@gmail.com>
2019-05-08 10:28:35 +0200

    no message

--- routes/studyservice.js
+++ routes/studyservice.js
@@ -32,6 +32,7 @@ router.get('/',function(req,res){
  * @returns {Error}  default - Unexpected error
  */
 router.post('/',function(req,res){
+    console.log(JSON.parse(req.body.researchers));
     //var listResearchers = JSON.stringify(req.body.researchers);
     //console.log(listResearchers);
     var listResearchers = [{name: "T"},{name: "A"}];
