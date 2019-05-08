9b728f881e937a761c7d54a1d24bd5831ec6c8dd
JustinVan <jtvan13520776@gmail.com>
2019-05-08 13:07:49 +0200

    no message

--- routes/encrypt.js
+++ routes/encrypt.js
@@ -6,7 +6,7 @@ router.get('/',function(req,res){
     var encryptedStr = cryptor.encryptWithRSAPublicKey("abc");
     res.send(encryptedStr.toString("base64"));
 });
-router.get('/decrypt',function(req,res){
+router.get('/decrypt/:id',function(req,res){
     var encryptedStr = req.params.id;
     var decryptedStr = cryptor.decryptWithRSAPrivateKey(encryptedStr);
     res.send(decryptedStr);
